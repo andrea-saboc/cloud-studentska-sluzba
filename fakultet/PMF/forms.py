30926a86d334c5490557239bab83fd07c3f250df
andrea-saboc <andrea_saboc@uns.ac.rs>
2023-01-25 00:52:44 +0100

    fix: nullable files

--- fakultet/PMF/forms.py
+++ fakultet/PMF/forms.py
@@ -8,7 +8,7 @@ class RegistrationStudentForm(forms.Form):
         surname = forms.CharField(label="Student's surname", max_length=100)
         jmbg = forms.CharField(label="JMBG", max_length=20)
         birthDate = forms.DateField()
-        image = forms.ImageField(label="image")
+        image = forms.ImageField(label="image", blank=True, null=True)
         indexNumber = forms.CharField(label="Index number", max_length=50)
 
 class RegistrationProfessorForm(forms.Form):
@@ -17,4 +17,4 @@ class RegistrationProfessorForm(forms.Form):
     surname = forms.CharField(label="Professor's surname", max_length=100)
     jmbg = forms.CharField(label="JMBG", max_length=20)
     bithDate = forms.DateField
-    image = forms.ImageField(label="image")
+    image = forms.ImageField(label="image", blank=True, null=True)
